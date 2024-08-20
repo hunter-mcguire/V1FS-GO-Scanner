a6a9fb0de83e6b2362d5f1a1a41421bc84dfe24b
Franz Fiorim <franzvitor@gmail.com>
2024-08-20 09:54:10 -0400

    adding linux arm

--- tools/build_binaries.sh
+++ tools/build_binaries.sh
@@ -1,51 +1,31 @@
 #!/bin/bash
-
+ 
 # Set the name of your main Go file
 MAIN_FILE="../main.go"
 OUTPUT_DIR="bin"
-
-# Check if Go is installed
-if ! command -v go &> /dev/null
-then
-    echo "Go is not installed. Please install Go and try again."
-    exit 1
-fi
-
+ 
 # Ensure the output directory exists
 mkdir -p $OUTPUT_DIR
-
-# Build for Linux
-echo "Building for Linux..."
+ 
+# Build for Linux amd64
+echo "Building for Linux amd64..."
 GOOS=linux GOARCH=amd64 go build -o $OUTPUT_DIR/v1_fs_scanner_linux $MAIN_FILE
-if [ $? -eq 0 ]; then
-    echo "Linux build completed successfully."
-else
-    echo "Linux build failed."
-fi
-
+ 
+# Build for Linux arm
+echo "Building for Linux ARM..."
+GOOS=linux GOARCH=arm64 go build -o $OUTPUT_DIR/v1_fs_scanner_linux_arm $MAIN_FILE
+ 
 # Build for Windows
 echo "Building for Windows..."
 GOOS=windows GOARCH=amd64 go build -o $OUTPUT_DIR/v1_fs_scanner_windows.exe $MAIN_FILE
-if [ $? -eq 0 ]; then
-    echo "Windows build completed successfully."
-else
-    echo "Windows build failed."
-fi
-
-# Build for macOS
-echo "Building for macOS..."
+ 
+# Build for macOS amd64
+echo "Building for macOS amd64..."
 GOOS=darwin GOARCH=amd64 go build -o $OUTPUT_DIR/v1_fs_scanner_macos $MAIN_FILE
-if [ $? -eq 0 ]; then
-    echo "macOS build completed successfully."
-else
-    echo "macOS build failed."
-fi
-
+ 
 # Build for macOS ARM
 echo "Building for macOS ARM..."
-GOOS=darwin GOARCH=arm64 go build -o $OUTPUT_DIR/v1_fs_scanner_macos-arm $MAIN_FILE
-if [ $? -eq 0 ]; then
-    echo "macOS ARM build completed successfully."
-else
-    echo "macOS ARM build failed."
-fi
+GOOS=darwin GOARCH=arm64 go build -o $OUTPUT_DIR/v1_fs_scanner_macos_arm $MAIN_FILE
+ 
+echo "Builds completed successfully."
+ 
